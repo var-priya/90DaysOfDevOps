bb053a0092e49c2ffb33e8eb10ed976679246a2f
Ubuntu <ubuntu@ip-172-31-33-127.eu-west-1.compute.internal>
2025-02-12 06:45:22 +0000

    part 4 completed

--- 2025/shell_scripting/list_all_user.sh
+++ 2025/shell_scripting/list_all_user.sh
@@ -4,8 +4,10 @@
 list_all_user(){
 	echo "=========================List the user==============================="
         awk -F: '{ print $1 ,$3 }' /etc/passwd
+
 }
-
-
-
-list_all_user
+	if [[ "$1" == "-l" || "$1" == "--list" ]]; then
+	    list_all_user
+	else 
+	   echo "Not a valid point. Please use -l or --list to get the list"
+	fi
